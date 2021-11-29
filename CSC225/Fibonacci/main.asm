06f565f3a1b83bd49a5ddc46ba8641e68c5cb7eb
Amanda2195 <92766569+Amanda2195@users.noreply.github.com>
2021-11-29 16:02:18 -0700

    11/29

--- CSC225/Fibonacci/main.asm
+++ CSC225/Fibonacci/main.asm
@@ -9,10 +9,10 @@ extern _ReadConsoleA@20: near
 
 .data
 
-n			dword		5
-prev		byte		1
-current		byte		1
-next		byte		0
+prev		dword		1		;*
+current		dword		1		;*
+next		dword		0		;*
+
 msg			byte		"Enter which number in the sequence you want to see: "
 
 .code
@@ -20,18 +20,28 @@ msg			byte		"Enter which number in the sequence you want to see: "
 main PROC near
 _main:
 
-mov		eax,	prev				; prev = 0
-mov		efx,	current				; current = 1
-mov		edx,	next				; next = 0
-mov		ecx,	n
+;expect to find n in eax when you start the program, at end result should be in eax
 
-loop:
+mov		eax,	5					;prev = 0		;*
+mov		ebx,	prev								;*
+mov		ecx,	current				; current = 1	;*
+mov		edx,	next				; next = 0		;*
+					
+;mov		ebx							;for user input
 
-add ebx, edx							; prev + curr
-mov eax, ebx							; next = prev + curr
-mov ebx, edx							; update prev with curr
-mov edx, eax							; update curr with next
-Loop loop
+mov		ecx,	1									;*
+
+_loop:
+
+cmp	ecx, eax							;*
+jge	exit								;*
+add ebx, ecx							; prev + curr
+mov edx, ebx							; next = prev + curr			;*not eax
+mov ebx, ecx							; update prev with curr
+mov esi, edx							; update curr with next
+inc	ecx									;*
+jmp _loop
+;Loop _loop
 
 	push 0
 	call _ExitProcess@4
